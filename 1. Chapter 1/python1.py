28dd8a2e406f3d725d76f21f97fb6c3067858a28
M Hamza Tahir <40276542+HamZil@users.noreply.github.com>
2021-09-09 19:36:41 +0500

    Delete 1. Chapter 1 directory

--- 1. Chapter 1/python1.py
+++
@@ -1,9 +0,0 @@
-from mod import sum
-def main() :
-    print("Hellow World")
-    a= int(input(" number a: "))
-    b= int(input(" number b: "))
-    print(sum(a,b))
-
-if __name__ == "__main__" :
-    main()
