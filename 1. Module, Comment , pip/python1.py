c1d7e15d867be4d4f5657376c5fe3bea71b7933c
M Hamza Tahir <40276542+HamZil@users.noreply.github.com>
2021-09-09 19:42:47 +0500

    Fundamentals 1

---
+++ 1. Module, Comment , pip/python1.py
@@ -0,0 +1,9 @@
+from mod import sum
+def main() :
+    print("Hellow World")
+    a= int(input(" number a: "))
+    b= int(input(" number b: "))
+    print(sum(a,b))
+
+if __name__ == "__main__" :
+    main()
